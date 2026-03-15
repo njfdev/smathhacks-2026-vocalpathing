8a85b2da56761be9dbfb4ecc0a6db9808a6eaac3
Blake Smith <Iornspider09@gmail.com>
2026-03-14 23:50:31 -0400

    Cool ahh model setup

--- ml_stuff/cool_ahh_model.py
+++ ml_stuff/cool_ahh_model.py
@@ -18,30 +18,30 @@ Shape: (batch, 50000)  ← 5 seconds * 10,000 samples
 #-------------------------
 
 #load audio file
-waveform, _ = librosa.load('your_audio.wav', sr=10000, mono=True)
-#automatiaclly scales to 10,000 hz audio and one channel
+waveform, _ = librosa.load('your_audio.wav', sr=10000, mono=True) #automatiaclly scales to 10,000 hz audio and one channel
 
+window_size = 50000 # 5 sec of 10,000 hz audio
 
-window_size = 50000 # 5 sec of 10,000 hz
 #chunk audio into the 5 sec windows
 chunks = []
 for i in range(0, len(waveform) - window_size, window_size):
     chunk = waveform[i : i + window_size]  # grab 50,000 samples
     chunks.append(chunk)
 
-batch=np.stack(chunks)
-audio=tf.constant(batch, dtype=tf.float32)
+batch=np.stack(chunks)#list of arrays into a 2d array
+audio=tf.constant(batch, dtype=tf.float32) #from np array to tensor
 
 #Run infrence
 @tf.function
 def get_embeddings(audio):
     return model(audio)
 
-outputs = get_embeddings(audio)
-outputs["scores"] = torch.sigmoid(torch.from_numpy(outputs["scores"].numpy()))
+outputs = get_embeddings(audio) #outputs is a dict of [embeddings and scores]
+outputs["scores"] = torch.sigmoid(torch.from_numpy(outputs["scores"].numpy())) #change to pytorch tensor and change to 0-1 probabalites
 
-class_idx = torch.argmax(outputs["scores"], dim=1)
+class_idx = torch.argmax(outputs["scores"], dim=1) #Max probaality is predicion
 
+#class mappings from google
 CLASS_NAMES = [
     'humpback_song',
     'humpback_call', 
@@ -57,10 +57,11 @@ CLASS_NAMES = [
     'nprw'
 ]
 
+#mapping our class index to the class names for each chunk and leaving them in all_class_predicitions
 all_class_predicions = []
-
 for i, idx in enumerate(class_idx):
     chunk_class = [CLASS_NAMES[idx.item()]]
     all_class_predicions.append(chunk_class)
 
+#changing to set type
 print("Unique classes detected:", set(all_class_predicions))
